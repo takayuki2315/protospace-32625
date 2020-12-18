43f9beea692103127975767455458d46fd5763b0
takayuki2315 <t.s151515@icloud.com>
2020-12-18 12:18:03 +0900

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,7 +15,6 @@ class CommentsController < ApplicationController
       @comments = @comment.prototype.includes(:user)
     end
 
-
   private
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
